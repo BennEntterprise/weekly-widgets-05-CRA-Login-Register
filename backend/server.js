f273b09de60548fb54e0f34696bf667826d0ddd3
BennEntterprise <kyle@bennentterprise.com>
2020-10-12 22:49:02 -0400

    adjust server to try and fix heroku

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ dotenv.config()
 
 // Connect to DB
 
-
 // Import Routes
 
 // Import Middlewares
@@ -21,34 +20,54 @@ const app = express()
 app.use(express.json())
 app.use('/', morgan('dev'))
 
-if(process.env.NODE_ENV ==='production'){
-    app.use(express.static(path.join(__dirname, 'frontend/build')))
-}else{
-    app.use(express.staticpath.join(__dirname,'frontend/public' ))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend/build')))
+  // Serve the Prod Single Page App
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend/build', 'index.html'))
+  })
+} else {
+  app.use(express.static(path.join(__dirname, 'frontend/public')))
+  // Serve the React File
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend/public', 'index.html'))
+  })
 }
 
-
 // Mount Routes
-app.use('/api/home', (req, res)=>{
-    res.json({message: 'Success', data: 'You also hit the GET /api/home in the ueHook'})
+app.use('/api/home', (req, res) => {
+  res.json({
+    message: 'Success',
+    data: 'You also hit the GET /api/home in the ueHook',
+  })
 })
 
-
-app.use('/api/login',(req, res) => {
-    res.json({message: 'Success', data: 'You hit hit the GET api/login in the ueHook'})
+app.use('/api/login', (req, res) => {
+  res.json({
+    message: 'Success',
+    data: 'You hit hit the GET api/login in the ueHook',
+  })
 })
 
-app.use('/api/register',(req, res) => {
-    res.json({message: 'Success', data: 'You hit hit the GET api/register in the ueHook'})
+app.use('/api/register', (req, res) => {
+  res.json({
+    message: 'Success',
+    data: 'You hit hit the GET api/register in the ueHook',
+  })
 })
 
-app.use('/api/dashboard',(req, res) => {
-    res.json({message: 'Success', data: 'You hit hit the GET api/dashboard in the ueHook'})
+app.use('/api/dashboard', (req, res) => {
+  res.json({
+    message: 'Success',
+    data: 'You hit hit the GET api/dashboard in the ueHook',
+  })
 })
 
 // Mount Error Middleware routes
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port: ${PORT} in ${process.env.NODE_ENV} mode`.green)
+app.listen(PORT, () => {
+  console.log(
+    `Server is running on port: ${PORT} in ${process.env.NODE_ENV} mode`.green
+  )
 })
