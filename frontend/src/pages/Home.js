636427eecf8c7c3a2042427bbeb59935dc2fe3a2
BennEntterprise <kyle@bennentterprise.com>
2020-10-14 13:43:18 -0400

    add some text to home to prompt login/register

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,17 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 const Home = () => {
-    useEffect(()=>{
-        axios.get('/api/home').then(res => console.log(res.data))
-      },[])
-    return (
-        <div>
-            Home
-        </div>
-    )
+  useEffect(() => {
+    axios.get('/api/home').then((res) => console.log(res.data))
+  }, [])
+  return (
+    <div>
+      You should get our awesome product. Either{' '}
+      <Link to='/register'>Register</Link> or <Link to='/login'>Login</Link> if
+      you already have an account.
+    </div>
+  )
 }
 
 export default Home
