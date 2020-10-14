0810d99c8af42e9fc04ebca63e61fb9d0799cf1d
BennEntterprise <kyle@bennentterprise.com>
2020-10-14 14:02:36 -0400

    basic form ui on login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,17 +1,44 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios'
-
+import { Form, Container, Row, Col, Button } from 'react-bootstrap'
 function Login() {
-    useEffect(()=>{
-        axios.get('/api/login').then(res => console.log(res.data))
-      },[])
+  useEffect(() => {
+    axios.get('/api/login').then((res) => console.log(res.data))
+  }, [])
 
-      
-    return (
-        <div>
-            Login
-        </div>
-    )
+  return (
+    <Container>
+      <Row>
+        <Col md={6}>
+          <Form>
+            <Form.Group controlId='email'>
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type='email'
+                placeholder='Enter Email'
+              ></Form.Control>
+              <Form.Text>
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId='password'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type='password'
+                placeholder='Enter Email'
+              ></Form.Control>
+              <Form.Text>
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Button variant='primary' type='submit'>
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default Login
